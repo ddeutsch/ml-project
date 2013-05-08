aadd16fcb6c55f267e16de3ec4d0e87efaa975bc
Ryan Cotterell <ryan@Ryans-MacBook-Air-2.local>
2013-05-08 08:24:11 -0400

    fixed enumerable bugs.
    added parametric versus non parametric switch
    
    TODO:
    
    MAP estimate for the covariance matrix

--- src/data/Cluster.java
+++ src/data/Cluster.java
@@ -18,6 +18,8 @@ public class Cluster
 	private static double kappa0 = 0;
 	private static double nu0 = 0;
 	
+	private ArrayRealVector[] data;
+	
 	public int id;
 	public ArrayRealVector mean = null;
 	public ArrayRealVector sampleMean = null;
@@ -27,71 +29,87 @@ public class Cluster
 
 	private MultivariateNormalDistribution F; 
 
+	
 	private ArrayRealVector sum = null;
 	private Array2DRowRealMatrix squareSum = new Array2DRowRealMatrix();
-	
+
 	private Array2DRowRealMatrix Psi0 = new Array2DRowRealMatrix();
 	
 	private static int D;
 	
-	public Cluster(ArrayRealVector mean, Array2DRowRealMatrix covariance, int D)
+	public Cluster(ArrayRealVector mean, Array2DRowRealMatrix covariance, int D, ArrayRealVector[] data)
 	{
+	
+		this.data = data;
 		this.id = counter++;
 		this.D = D;
-		
 		this.mean = mean;
 		this.covariances = covariance;
 		
 		Psi0 = new Array2DRowRealMatrix(GMM.identityMatrix(D));
-		Psi0 = (Array2DRowRealMatrix) Psi0.scalarMultiply(10);
-		
-		this.F = new MultivariateNormalDistribution(mean.getDataRef(), covariances.getData());
+		Psi0 = (Array2DRowRealMatrix) Psi0.scalarMultiply(1);
 		
 		this.sampleMean = new ArrayRealVector(D);
 		this.sum = new ArrayRealVector(this.mean.getDimension());
 		this.squareSum = new Array2DRowRealMatrix(D,D);
+		
+		this.F = new MultivariateNormalDistribution(mean.getDataRef(), covariances.getData());
+		
+	
 	}
 	
 	public void add(int n, ArrayRealVector x) {
-		sampleMean = (ArrayRealVector) sampleMean.mapMultiplyToSelf(this.points.size()).add(x).mapDivideToSelf(this.points.size() + 1);
+	
+		sampleMean = (ArrayRealVector) ((sampleMean.mapMultiply(this.points.size())).add(x)).mapDivide(this.points.size() + 1);
 		squareSum = (Array2DRowRealMatrix) squareSum.add(x.outerProduct(x));
 		sum = sum.add(x);
+			
 		this.points.add(n);
+		recomputeSS();
 	}
 	
-	private void recomputeSS()
+	public void recomputeSS()
 	{
 		
-		mean = (ArrayRealVector) ( GMM.baseMean.mapMultiplyToSelf(kappa0).add(sampleMean.mapMultiplyToSelf(this.points.size())))
-				.mapDivideToSelf((kappa0 + this.points.size()));
-		double kappaN = kappa0 + points.size(); 
+		int n = points.size();
 		
-		double nu = nu0 + points.size();
-		/*
-		 * 
-		 * C = self._square_sum - self.n_points * (mu.transpose() * mu)
-        Psi = (self._Psi_0 + C + self._kappa_0 * self.n_points
-             * mu_mu_0.transpose() * mu_mu_0 / (self._kappa_0 + self.n_points))
+		if (n == 0) {
+			mean = GMM.baseMean.copy();
+			sum = new ArrayRealVector(D);
+			squareSum = new Array2DRowRealMatrix(D,D);
+			sampleMean = new ArrayRealVector(D);
+		} else {
+			
+			mean =  (ArrayRealVector) (( GMM.baseMean.copy().mapMultiply(kappa0)).add(sampleMean.mapMultiply(this.points.size()))).mapDivide((kappa0 + this.points.size()));
+			
+			
+			double kappaN = kappa0 + points.size(); 
 
-        self.mean = ((self._kappa_0 * self._mu_0 + self.n_points * mu) 
-                    / (self._kappa_0 + self.n_points))
-        self.covar = (Psi * (kappa_n + 1)) / (kappa_n * (nu - self.n_var + 1))
-		 */
+			double nu = nu0 + points.size();
+			
+			Array2DRowRealMatrix mu =  (Array2DRowRealMatrix) (sum.outerProduct(sum)).scalarMultiply(n);
+			Array2DRowRealMatrix C = (Array2DRowRealMatrix) squareSum.subtract( mu);
+			Array2DRowRealMatrix PsiN = Psi0.add(C);
+
+			System.out.println(PsiN);
+			this.covariances = PsiN;
+		}
+			
+		this.F = new MultivariateNormalDistribution(mean.getDataRef(), covariances.getData());
+
 		
-		ArrayRealVector xMu  = sampleMean.subtract(GMM.baseMean);
-		Array2DRowRealMatrix C = (Array2DRowRealMatrix) squareSum.subtract( GMM.baseMean.outerProduct(GMM.baseMean).scalarAdd(this.points.size()));
-		Array2DRowRealMatrix PsiN = (Array2DRowRealMatrix) xMu.outerProduct(xMu).scalarMultiply((kappaN + 1) / ( kappaN));
-				
-		PsiN = PsiN.add(Psi0.add(C));
 		
-		this.covariances = (Array2DRowRealMatrix) PsiN.scalarMultiply((kappaN + 1) / (kappaN * (nu - D + 1)));
 	}
 	
 	public void remove(int n, ArrayRealVector x) {
-		sampleMean = (ArrayRealVector) sampleMean.mapMultiplyToSelf(this.points.size()).subtract(x).mapDivideToSelf(this.points.size() - 1);
+	
+		sampleMean = (ArrayRealVector) ((sampleMean.mapMultiply(this.points.size())).subtract(x)).mapDivide(this.points.size() - 1);
 		squareSum = (Array2DRowRealMatrix) squareSum.subtract(x.outerProduct(x));
 		sum = sum.subtract(x);
 		this.points.remove(n);
+	
+		
+		recomputeSS();
 	}
 	
 	public double pdf(ArrayRealVector x) {
--- src/driver/GMM.java
+++ src/driver/GMM.java
@@ -3,9 +3,12 @@ package driver;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 import org.apache.commons.math3.distribution.MultivariateNormalDistribution;
@@ -32,10 +35,10 @@ public class GMM
 	public static ArrayRealVector baseMean;
 	private static Array2DRowRealMatrix baseCovariance;
 
-	private static double alpha = 0.5;
+	private static double alpha = 1.0;
 	
 	private static MultivariateNormalDistribution baseDistribution;
-	private static int iterations = 1000;
+	private static int iterations = 100;
 	
 	public static void main(String[] args) 
 	{
@@ -45,55 +48,127 @@ public class GMM
 		baseMean = new ArrayRealVector(D);
 		//initialize covariance to identity
 		baseCovariance = new Array2DRowRealMatrix(identityMatrix(D)); 
+		baseCovariance = (Array2DRowRealMatrix) baseCovariance.scalarMultiply(1);
 		
 		baseDistribution = new MultivariateNormalDistribution(baseMean.getDataRef(),baseCovariance.getData());
 		
 		// add first cluster
-		Cluster c = new Cluster(baseMean, baseCovariance, D);
-		clusters.put(c.id, c);
-		K = 1;
 		
-		// add all points to 0th cluster
-		for (int i = 0; i < data.length; i++)
-			c.add(i, data[i]);
+		boolean nonparametric = false;
 		
+		K = 10;
+		for (int k = 0; k < K; ++k) {
+			Cluster c = new Cluster(baseMean, baseCovariance, D, data);
+			clusters.put(c.id, c);
+		}
+		
+		Random rand = new Random();
+		//Randomly assign points to a cluster
+		for (int i = 0; i < data.length; i++) {
+		//	System.out.println(data[i]);\
+			int k = rand.nextInt(K);
+			clusters.get(k).add(i, data[i]);
+			Z[i] = k;
+		}
+		
+		for (int k : clusters.keySet()) {
+			Cluster c = clusters.get(k);
+			System.out.println(c.mean);
+		}
+		
+		if (nonparametric) {
+			List<Integer> removable = new LinkedList<Integer>(); // to avoid concurrent modification exception
+			for (int k : clusters.keySet()) {
+				Cluster c = clusters.get(k);
+				if (c.points.size() == 0) {
+					removable.add(k);
+				}
+			}
+			for (int k : removable) {
+				clusters.remove(k);
+			}
+		}
+		
+		
+		//System.exit(0);
 		for (int t = 0; t < iterations; t++)
 		{
-			System.out.println(t + ": " + clusters.size());
-			System.out.println(likelihood());
+			
+			
 			for (int n = 0; n < N; ++n) {
-	 			Cluster curCluster = clusters.get(Z[n]); // SLOW 
+				
+				
+				Cluster curCluster = clusters.get(Z[n]); // SLOW 
 				curCluster.remove(n,data[n]); // SLOW
 				
-				if (curCluster.points.size() == 0)
-					clusters.remove(Z[n]);
-				
+				if (curCluster.points.size() == 0)  {
+					if (nonparametric) {
+						clusters.remove(Z[n]);				
+					}
+				}
+
 				List<Integer> clusterOrder = new ArrayList<Integer>();
-				double[] probs = new double[clusters.size() + 1];
-				int i = 0;
-				for (int key : clusters.keySet()) {
-					clusterOrder.add(key);
-					probs[i] = (((double) ( curCluster.points.size() )) / ( alpha + N - 1)) * curCluster.pdf(data[n]); 
+				
+				double[] probs;
+				if (nonparametric) {
+					probs = new double[clusters.size() + 1];
+				} else {
+					probs = new double[clusters.size()];
 				}
 				
+				int i = 0;
+				for (int key : clusters.keySet()) {
+					Cluster tmp = clusters.get(key);
+					clusterOrder.add(key);
+					if (nonparametric) {
+						probs[i] = (((double) ( tmp.points.size() )) / ( alpha + N - 1)) * tmp.pdf(data[n]); 
+					} else {
+						
+						probs[i] = (((double) ( tmp.points.size()  + ((double) alpha) / K )) / ( alpha + N - 1)) * tmp.pdf(data[n]); 
+
+					}
+					++i;
+				}
+
 				//MAP ESTIMATE OF THE PRIOR. NOT EVALUATING PRIOR PREDICTIVE
-				probs[clusters.size()] = ( ( double ) alpha / (alpha + N - 1 )) * baseDistribution.density(data[n].getDataRef());
-				
+				if (nonparametric ) {
+					probs[clusters.size()] = ( ( double ) alpha / (alpha + N - 1 )) * baseDistribution.density(data[n].getDataRef());
+				}
+					
+
 				int sample = new Multinomial(probs).sample();
 				//new cluster
-				if (sample == clusters.size()) {
+				if (nonparametric && sample == clusters.size()) {
 
-					Cluster cluster = new Cluster(baseMean, baseCovariance, D);
+					Cluster cluster = new Cluster(baseMean, baseCovariance, D, data);
 					cluster.add(n, data[n]);
 					clusters.put(cluster.id, cluster);
+					Z[n] = cluster.id;
 				} else {
 					//old cluster
 					int clusterId = clusterOrder.get(sample);
 					Cluster newCluster = clusters.get(clusterId);
+					
+
 					newCluster.add(n, data[n]);
+
+					Z[n] = clusterId;
 				}
+			
+				
 			}
+		
+			
 		}
+		System.out.println("FINAL");
+		for (int key : clusters.keySet()) {
+			Cluster clus = clusters.get(key);
+			System.out.println("Mean\t" + clus.mean);
+			System.out.println(clus.points.size());
+			System.out.println();
+		}
+		
+
 	}
 	
 	private static double likelihood()
@@ -103,6 +178,7 @@ public class GMM
 		for (int n = 0; n < data.length; n++)
 		{
 			Cluster cluster = clusters.get(Z[n]);
+			//System.out.println(cluster.covariances);
 			LUDecomposition det = new LUDecomposition(cluster.covariances);
 			
 			likelihood -= (0.5 * N * Math.log(2.0 * Math.PI) + 0.5 * Math.log(det.getDeterminant()));
@@ -112,6 +188,7 @@ public class GMM
 			likelihood -= 0.5 * mean_var.dotProduct(det.getSolver().getInverse().operate(mean_var));
 			// TODO add the influence of n_components
 		}
+	//	System.out.println();
 		
 		return likelihood;
 		
@@ -151,12 +228,12 @@ public class GMM
 			{
 				split = scanner.nextLine().split(" ");
 				double[] array = new double[split.length - 1];
-				
 				for (int i = 0; i < array.length; i++)
 					array[i] = Double.parseDouble(split[i + 1]);
 				
 				data[n] = new ArrayRealVector(array);
 				++n;
+				
 			}
 			
 			Z = new int[data.length];
